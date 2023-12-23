5f74b85260c9bf37b4437f04c51c82b284cddf04
sandeshm2 <46280009+sandeshm2@users.noreply.github.com>
2023-12-23 05:34:23 +0000

    01-variables done

---
+++ 01_basic/01_vaiable.js
@@ -0,0 +1,14 @@
+const userName = "Sandesh";
+let password = "12345";
+var confirmPassword = "12345";
+city = "mumbai";
+
+city = "jaipur";
+
+console.table([userName, password, confirmPassword, city]);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/

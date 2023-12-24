2868e0aaab7e304b8615235a7f1dae756d7e2cfa
sandeshm2 <46280009+sandeshm2@users.noreply.github.com>
2023-12-24 02:26:54 +0000

    string

---
+++ 01_basic/06_stringMethods.js
@@ -0,0 +1,33 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+
+
+//++++++++++++++++++++++++++++ String Method ++++++++++++++++++++++++++
+
+// String length                        // String slice()
+// String substring()                   // String substr()
+// String replace()                     // String replaceAll()
+// String toUpperCase()                 // String toLowerCase()
+// String concat()                      // String trim()
+// String trimStart()                   // String trimEnd()
+// String padStart()                    // String padEnd()
+// String charAt()                      // String charCodeAt()
+// String split()
+
+let lenghtText = "abcedefegfkjasfjs";   //The length property returns the length of a string:
+console.log(lenghtText.length);
+
+let text = "Apple, Banana, Kiwi"; //slice() string एक भाग काढतो आणि काढलेला भाग नवीन string मध्ये परत करतो.
+console.log(text.slice(7,13)); //The method takes 2 parameters: start position, and end position (end not included).
+
